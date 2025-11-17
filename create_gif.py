838d911c75ac6a3f33a553ba6ee6615580d977df
Optimus1704 <sebasgjc@gmail.com>
2025-11-17 21:58:26 +0100

    Create create_gif.py

---
+++ create_gif.py
@@ -0,0 +1,21 @@
+# Final project with Python
+
+# Create a GIF with Python
+
+
+# GIF with two images
+
+# In this case I didn't have any issues using the two images.
+
+import imageio.v3 as iio
+
+filenames = ['chicklet1.png', 'chicklet2.png']
+images = [ ]
+
+for filename in filenames:
+  images.append(iio.imread(filename))
+
+iio.imwrite('chicklet.gif', images, duration = 500, loop = 0) 
+
+# I set the duration to 500 milliseconds because 
+# I believe it's the right value to maintain a smooth transition between the two images.
